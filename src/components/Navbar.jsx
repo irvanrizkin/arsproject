e65eed9c0cb5e3bfe44d3ab0ba3cd0b8e8069452
Muh. Afdal Maulana Said <afdalmaulanaaa@gmail.com>
2024-03-15 02:42:04 +0800

    sticky navbar, adding logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,55 @@
+import { useEffect, useState } from "react";
 import { navItems } from "../constant";
+import logo from "../assets/logo.png";
+import logoWarna from "../assets/logoWarna.png";
 export const Navbar = () => {
+  const [isSticky, setIsSticky] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
   console.log(navItems);
   return (
-    <div className="w-full flex justify-end py-10 pr-[9.5rem] z-10 absolute">
-      <ul className="flex space-x-8 py-4 text-white">
-        {navItems.map((item) => (
-          <li key={item.path}>
-            <a href={`#${item.path}`}>{item.name}</a>
-          </li>
-        ))}
-      </ul>
-      <div>
-        <button className="bg-[#f98801] text-white px-4 py-3 mt-1 ml-5">
-          Hubungi Sekarang
-        </button>
-      </div>
-    </div>
+    <header className="w-full fixed top-0 left-0 right-0 text-white bg-white md:bg-transparent z-20">
+      <nav
+        className={`py-10 ${
+          isSticky
+            ? "sticky top-0 left-0 py-2 right-0 border-b text-black bg-white duration-300"
+            : ""
+        }`}
+      >
+        <div className="w-full flex justify-evenly py-2 pr-[9.5rem] z-10">
+          <div className="ml-[6rem]">
+            {isSticky ? (
+              <img src={logoWarna} alt="logo" />
+            ) : (
+              <img src={logo} alt="logo" />
+            )}
+            {/* <img src={logo} alt="logo" /> */}
+          </div>
+          <ul className="flex space-x-8 py-4 ml-[25rem]">
+            {navItems.map((item) => (
+              <li key={item.path}>
+                <a href={`#${item.path}`}>{item.name}</a>
+              </li>
+            ))}
+          </ul>
+          <div>
+            <button className="bg-[#f98801] text-white px-4 py-3 mt-1 ml-5">
+              Hubungi Sekarang
+            </button>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
